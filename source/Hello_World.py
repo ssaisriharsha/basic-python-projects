9e0a90633780482576a08bce4ce94dbb958eb475
programmer24680 <69705177+programmer24680@users.noreply.github.com>
2020-09-23 14:51:58 +0530

    Add files via upload

---
+++ source/Hello_World.py
@@ -0,0 +1,12 @@
+'''
+project name: Hello world
+target user: Me
+target system: WiNdOwS 10
+expectation: computer should print hello world
+
+'''
+import time
+print("Hello\nWorld!")
+time.sleep(7)
+input("Enter any letter and press enter to exit: ")
+exit()
---
+++ source/John_smith.py
@@ -0,0 +1,8 @@
+#This is from Mosh python video
+
+Name = "john_smith"
+age = 20
+is_new_patient = True
+
+Name = input("what is your name?: ")
+print("Hello " + Name + str(age))
