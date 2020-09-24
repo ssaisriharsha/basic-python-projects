cc9eca2886c2becbf83d2e106fa5a104cc88055f
programmer24680 <69705177+programmer24680@users.noreply.github.com>
2020-09-24 07:08:08 +0530

    Add files via upload
    This calculator is meant for addition purpose only.

---
+++ source/Basic_calculator.py
@@ -0,0 +1,11 @@
+#This is a Basic Calculator
+#It includes only addition function
+from time import*
+print("This calculator is only for addition purpose.")
+sleep(2)
+print("Please wait while the calculator initializes.")
+sleep(3)
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+print("The result is: " + str(num1 + num2))
+sleep(3)
